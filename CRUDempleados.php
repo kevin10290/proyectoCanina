b174fd2b6a06148b1fdbad3cdc1f58190f19cc2c
kevin alzate <kevinalzate10290@gmail.com>
2024-02-21 11:13:40 -0500

    reorganizacion

--- CRUDempleados.php
+++ CRUDempleados.php
@@ -2,6 +2,23 @@
 
 $sql= "SELECT * FROM empleado";
 
-$result= $sql
+$result= $conexion->query($sql);
+
+//validamos para mostrar datos
+
+if ($resultado->num_rows > 0) {
+
+    // hay información que mostrar
+
+    while ($row = $resultado->fetch_assoc()) {
+
+        echo "<hr> id asociado: " . $row["id"] . "-Nombre Usuario: " . $row["nombre"] . " " . $row["apellido"] . "<hr>";
+    }
+} else {
+
+    echo "Sin información ingresada aún";
+}
+
+$conexion->close();
 
 ?>
