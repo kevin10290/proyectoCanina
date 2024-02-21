fcc95f0c9cc98917542b8fade6b9172d1df94123
kevin alzate <kevinalzate10290@gmail.com>
2024-02-21 09:18:27 -0500

    Actualización de archivos html

--- assets/demo/chart-bar-demo.js
+++ assets/demo/chart-bar-demo.js
@@ -7,11 +7,11 @@ var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"],
     datasets: [{
       label: "Revenue",
-      backgroundColor: "rgba(2,117,216,1)",
-      borderColor: "rgba(2,117,216,1)",
+      backgroundColor: "rgba(225,78,74,1)",
+      borderColor: "rgba(189,66,62,1)",
       data: [4215, 5312, 6251, 7841, 9821, 14984],
     }],
   },
